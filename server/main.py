2f22ab8b32f44332abf1560c5c16bb6fa60edab7
ajmalrasi <kpajmalrasi@gmail.com>
2024-05-04 00:11:25 +0530

    Image open

--- server/main.py
+++ server/main.py
@@ -29,13 +29,12 @@ async def process_image(data: dict = Body(...)):
         if is_raspberry_pi:
             _, scale = common.set_resized_input(
                 interpreter, image.size, lambda size: image.resize(size, Image.LANCZOS))
-            
-        start = time.perf_counter()
-        interpreter.invoke()
-        inference_time = time.perf_counter() - start
-        objs = detect.get_objects(interpreter, 0.4, scale)
-        print('%.2f ms' % (inference_time * 1000))
-        print(objs)
+            start = time.perf_counter()
+            interpreter.invoke()
+            inference_time = time.perf_counter() - start
+            objs = detect.get_objects(interpreter, 0.4, scale)
+            print('%.2f ms' % (inference_time * 1000))
+            print(objs)
 
         return JSONResponse(content={"message": "Image received and processed"})
     except Exception as e:
