05197122f1173129ed864d9b9ae2c80b9c4f75a5
ajmalrasi <kpajmalrasi@gmail.com>
2024-05-04 00:35:51 +0530

    Clean up

--- client/client.py
+++ client/client.py
@@ -2,11 +2,10 @@ import cv2
 import requests
 import base64
 
-# Server URL (replace with your actual server address and port)
 url = 'http://192.168.3.20:8000/predict'
 
-# Initialize video capture from webcam
-cap = cv2.VideoCapture(0)  # 0 usually refers to the default webcam
+
+cap = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cap.read()
@@ -22,7 +21,6 @@ while True:
         "image": jpg_as_text
     }
 
-    # Send the POST request to the server
     response = requests.post(url, json=data)
 
     if response.status_code == 200:
@@ -42,14 +40,10 @@ while True:
     else:
         print(f"Error sending image: {response.status_code} - {response.text}")
 
-    # Show the frame (optional, for debugging)
     cv2.imshow('Webcam', frame)
     cv2.namedWindow('Webcam', cv2.WINDOW_NORMAL)
-    # break
-    # Exit on 'q' key press
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
