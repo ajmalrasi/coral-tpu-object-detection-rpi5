c5ec9c6023530a96539a7e910ee9e6c24b9eb7d2
ajmalrasi <kpajmalrasi@gmail.com>
2024-05-05 13:36:03 +0530

    Efficientdet lite1 model

--- server/utils.py
+++ server/utils.py
@@ -5,6 +5,6 @@ from pycoral.utils.edgetpu import make_interpreter
 
 def load_model():
     labels = read_label_file('models/coco_labels.txt')
-    interpreter = make_interpreter('models/ssd_mobilenet_v2_coco_quant_postprocess_edgetpu.tflite')
+    interpreter = make_interpreter('models/efficientdet_lite1_384_ptq_edgetpu.tflite')
     interpreter.allocate_tensors()
     return interpreter, labels
